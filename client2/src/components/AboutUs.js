07e1ff9313ae14e5a0a031332bd1da4afa984b3a
Mazianos <mazin.marwan@gmail.com>
2021-05-13 22:46:20 -0700

    WHY WONT THE GOD DAMNED IMAGES SHOW ON THE F!@#$ING ABOUT US PAGE????

--- client2/src/components/AboutUs.js
+++ client2/src/components/AboutUs.js
@@ -1,13 +1,63 @@
 import React from 'react';
+import { Card, Container } from 'react-bootstrap';
 import "../css/about-us.css";
 import Navbar from './Navbar';
+import {jasonFiltered, jLauFiltered, rayFiltered, mazFiltered} from '../images/images'
 
 export default function AboutUs() {
     return (
         <>
             <Navbar/>
-            <TextBlock></TextBlock>
-            
+            {/* <TextBlock></TextBlock> */}
+            <Container>
+                <Card>
+                    <a href="https://www.linkedin.com/in/rwong97/">
+                        <img src={rayFiltered} class="card-img-top" alt="https://www.linkedin.com/in/rwong97/"/>
+                    </a>
+                    <Card.Body>
+                        <Card.Title>Raymond Wong</Card.Title>
+                        <Card.Text>Ray is a business graduate pursuing a tech career. His specializations are HTML/CSS
+          and JavaScript. He has a passion for giving back and works with Burnaby Neighbourhood House as a
+          digital literacy volunteer.</Card.Text>
+                    </Card.Body>
+                </Card>
+
+                <Card>
+                    <a href="https://www.linkedin.com/in/jasonja-ahn/">
+                        <img src={jasonFiltered} class="card-img-top" alt="https://www.linkedin.com/in/jasonja-ahn/"/>
+                    </a>
+                    <Card.Body>
+                        <Card.Title>Jason Ahn</Card.Title>
+                        <Card.Text>Jason Ahn is an aspiring software engineer with a Bachelor's degree in Criminology from
+          Simon Fraser University. Born in South Korea and raised in British Columbia, Canada, Jason currently resides
+          in Vancouver. In his free time, he enjoys eating lasagna like Garfield.</Card.Text>
+                    </Card.Body>
+                </Card>
+
+                <Card>
+                    <a href="https://www.linkedin.com/in/johnsonlau/">
+                        <img src={jLauFiltered} class="card-img-top" alt="https://www.linkedin.com/in/johnsonlau/"/>
+                    </a>
+                    <Card.Body>
+                        <Card.Title>Johnson Lau</Card.Title>
+                        <Card.Text>Johnson Lau is a young software developer looking to improve his skills in the field. Fresh
+          from highschool, he is ready to make a lasting impact through his work. Johnson loves to play volleyball, and
+          cook during his free time.</Card.Text>
+                    </Card.Body>
+                </Card>
+
+                <Card>
+                    <a href="https://www.linkedin.com/in/mazin-marwan-0b3409154/">
+                        <img src={mazFiltered} class="card-img-top" alt="https://www.linkedin.com/in/mazin-marwan-0b3409154/"/>
+                    </a>
+                    <Card.Body>
+                        <Card.Title>Mazin Marwan</Card.Title>
+                        <Card.Text>Mazin Marwan is a software developer from Vancouver BC seeking to create interesting and
+          interactive applications focused on simplifying workflows for himself and those around him. Mazin loves to
+          play various roleplaying games such as Dungeons and Dragons and write when he has the time.</Card.Text>
+                    </Card.Body>
+                </Card>
+            </Container>
         </>
     )
 }
--- client2/src/components/App.js
+++ client2/src/components/App.js
@@ -3,9 +3,10 @@ import { AuthProvider } from "../contexts/AuthContext";
 import Signup from "./Signup";
 import { Container } from "react-bootstrap";
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import LandingPage from "./LandingPage";
+// import LandingPage from "./LandingPage";
 import Login from "./Login";
 import PrivateRoute from './PrivateRoute';
+import AboutUs from "./AboutUs";
 
 function App() {
     return (
@@ -13,9 +14,10 @@ function App() {
             <Router>
             <AuthProvider>
                 <Switch>
-                    <PrivateRoute exact path="/" component={LandingPage}/>
+                    {/* <PrivateRoute exact path="/" component={LandingPage}/> */}
                     <Route path="/signup" component={Signup}/>
                     <Route path="/login" component={Login}/>
+                    <Route path="/aboutUs" component={AboutUs}/>
                 </Switch>
             </AuthProvider>
             </Router>
