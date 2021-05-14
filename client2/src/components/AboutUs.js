3ce364bbb39d29e7a554e297cc9d4d5b796c80c9
Mazianos <mazin.marwan@gmail.com>
2021-05-13 22:23:47 -0700

    Starting to integrate aboutUs.html.

---
+++ client2/src/components/AboutUs.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "../css/about-us.css";
+import Navbar from './Navbar';
+
+export default function AboutUs() {
+    return (
+        <>
+            <Navbar/>
+            <TextBlock></TextBlock>
+            
+        </>
+    )
+}
