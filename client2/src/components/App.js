9a836ea0f01e2dfe4613acc10b115f407ef25cb0
Jason Ahn <JasonJA.Ahn@gmail.com>
2021-05-14 01:17:40 -0700

    changed the file path and deleted p tag

--- client2/src/components/App.js
+++ client2/src/components/App.js
@@ -11,7 +11,6 @@ import AboutUs from "./AboutUs";
 function App() {
     return (
         <Container>
-            <p>HELLO WORLD GET ME DONER</p>
             <Router>
             <AuthProvider>
                 <Switch>
--- client2/src/index.js
+++ client2/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import App from './components/App';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 ReactDom.render(
   <React.StrictMode>
