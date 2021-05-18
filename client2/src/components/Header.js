b5307b1ac1e60813ee24866bb922841d45baf950
Jason Ahn <JasonJA.Ahn@gmail.com>
2021-05-17 20:57:42 -0700

    implemented left fade-in navigation bar

--- client2/src/components/Header.js
+++ client2/src/components/Header.js
@@ -6,6 +6,26 @@ import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import { Link as Scroll } from 'react-scroll'
 
 
+/*
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import Fade from '@material-ui/core/Fade';
+*/
+
+import clsx from 'clsx';
+import Drawer from '@material-ui/core/Drawer';
+import Button from '@material-ui/core/Button';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+import AccountBoxIcon from '@material-ui/icons/AccountBox';
+
+
 const useStyles = makeStyles((theme) => ({
     root: {
         display: 'flex',
@@ -14,25 +34,25 @@ const useStyles = makeStyles((theme) => ({
         height: "100vh",
         fontFamily: "Roboto",
     },
-    appbar:{
-        background:"none",
+    appbar: {
+        background: "none",
     },
     appbarWrapper: {
         width: "80%",
         margin: "0 auto"
     },
-    appbarTitle:{
-        
+    appbarTitle: {
+
         fontSize: "2rem",
-        
+
         flexGrow: '1',
     },
-    icon:{
+    icon: {
         color: '#fff',
         fontSize: "2rem",
     },
     colorText: {
-        color:"#5AFF3D",
+        color: "#5AFF3D",
     },
     container: {
         textAlign: "center",
@@ -45,42 +65,134 @@ const useStyles = makeStyles((theme) => ({
         color: '#5AFF3D',
         fontSize: '4rem',
     },
+    list: { //NEW
+        width: 250,
+    },
+    fullList: {
+        width: 'auto',
+    },
 }));
 
 export default function Header() {
     const classes = useStyles();
     const [checked, setChecked] = useState(false);
-    useEffect(()=>{
+    useEffect(() => {
         setChecked(true);
-    },[])
-    return ( 
-    <div className = {classes.root} id = "header">
-        <AppBar className = {classes.appbar} elevation={0}>
-            <Toolbar className = {classes.appbarWrapper}>
-                <h1 className = {classes.appbarTitle}>
-                    We<span className = {classes.colorText}>cycle</span>
-                </h1>
-                <IconButton>
-                    <SortIcon className = {classes.icon} />
-                </IconButton>
-            </Toolbar>
-        </AppBar>
+    }, [])
 
-        <Collapse 
-            in={checked} {...(checked ? { timeout: 1000 } : {})} 
-            collapsedHeight={50}
-        > 
-            <div className = {classes.container}>
-                <h1 className={classes.title}> 
-                    Meet the <br/> <span className = {classes.colorText}>Team </span>
-                </h1>
-                <Scroll to = "place-to-visit" smooth={true}>
+    // NEW
+
+    const [state, setState] = React.useState({
+        right: false,
+    });
+
+    const toggleDrawer = (anchor, open) => (event) => {
+        if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+            return;
+        }
+
+        setState({ ...state, [anchor]: open });
+    };
+
+    const list = (anchor) => (
+        <div
+            className={clsx(classes.list, {
+                [classes.fullList]: anchor === 'top' || anchor === 'bottom',
+            })}
+            role="presentation"
+            onClick={toggleDrawer(anchor, false)}
+            onKeyDown={toggleDrawer(anchor, false)}
+        >
+            <List>
+                {['Profile', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+                    <ListItem button key={text}>
+                        <ListItemIcon>{index % 2 === 0 ? <AccountBoxIcon /> : <MailIcon />}</ListItemIcon>
+                        <ListItemText primary={text} />
+                    </ListItem>
+                ))}
+            </List>
+            <Divider />
+            <List>
+                {['All mail', 'Trash', 'Spam'].map((text, index) => (
+                    <ListItem button key={text}>
+                        <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                        <ListItemText primary={text} />
+                    </ListItem>
+                ))}
+            </List>
+        </div>
+    );
+
+
+    /*
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+    */
+
+    return (
+        <div className={classes.root} id="header">
+            <AppBar className={classes.appbar} elevation={0}>
+                <Toolbar className={classes.appbarWrapper}>
+                    <h1 className={classes.appbarTitle}>
+                        We<span className={classes.colorText}>cycle</span>
+                    </h1>
                     <IconButton>
-                        <ExpandMoreIcon className = {classes.goDown} />
+                        
+                        {['right'].map((anchor) => (
+                            
+                            <React.Fragment key={anchor}>
+                                <IconButton onClick={toggleDrawer(anchor, true)}>
+                                    <SortIcon className={classes.icon} />
+                                </IconButton>
+                                
+                                <Drawer anchor={anchor} open={state[anchor]} onClose={toggleDrawer(anchor, false)}>
+                                    {list(anchor)}
+                                </Drawer>
+                            </React.Fragment>
+                        ))}
                     </IconButton>
-                </Scroll>
-            </div>
-        </Collapse>
-    </div>
+
+
+
+                    {/* <Menu
+                        id="fade-menu"
+                        anchorEl={anchorEl}
+                        keepMounted
+                        open={open}
+                        onClose={handleClose}
+                        TransitionComponent={Fade}
+                    >
+                        <MenuItem onClick={handleClose}>Profile</MenuItem>
+                        <MenuItem onClick={handleClose}>My account</MenuItem>
+                        <MenuItem onClick={handleClose}>Logout</MenuItem>
+                    </Menu> */}
+
+                </Toolbar>
+            </AppBar>
+
+            <Collapse
+                in={checked} {...(checked ? { timeout: 1000 } : {})}
+                collapsedHeight={50}
+            >
+                <div className={classes.container}>
+                    <h1 className={classes.title}>
+                        Meet the <br /> <span className={classes.colorText}>Team </span>
+                    </h1>
+                    <Scroll to="place-to-visit" smooth={true}>
+                        <IconButton>
+                            <ExpandMoreIcon className={classes.goDown} />
+                        </IconButton>
+                    </Scroll>
+                </div>
+            </Collapse>
+        </div>
     );
 }
