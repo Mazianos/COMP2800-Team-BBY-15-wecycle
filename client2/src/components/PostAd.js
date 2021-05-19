6b680188ef1cc703c2d5b66566b365df52f4ab1e
Yomamajohn <yichenglau@gmail.com>
2021-05-19 00:37:04 -0700

    Changed postAd to material UI, have not hooked up to backend

--- client2/src/components/PostAd.js
+++ client2/src/components/PostAd.js
@@ -1,7 +1,55 @@
 import React, { useRef, useState } from 'react';
-import { Card, Form, Button } from 'react-bootstrap';
+import { Card, Form } from 'react-bootstrap';
 import { useHistory } from 'react-router-dom';
 import $ from 'jquery';
+//import cssClasses from '*.module.css';
+
+
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+
+
+
+
+function Copyright() {
+    return (
+      <Typography variant="body2" color="textSecondary" align="center">
+        {'Copyright © '}
+        <Link color="inherit" href="https://material-ui.com/">
+          WeCycle
+        </Link>{'.com '}
+        {new Date().getFullYear()}
+        {'.'}
+      </Typography>
+    );
+  }
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+      marginTop: theme.spacing(8),
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+    form: {
+      width: '100%', // Fix IE 11 issue.
+      marginTop: theme.spacing(1),
+    },
+    submit: {
+      margin: theme.spacing(3, 0, 2),
+    },
+    upload : {
+        margin: theme.spacing(1)
+    }
+  }));
 
 export default function PostAd() {
     const titleRef = useRef();
@@ -19,6 +67,7 @@ export default function PostAd() {
     const [loading, setLoading] = useState(false);
     const currentUser = useState();
     const history = useHistory();
+    const classes = useStyles();
 
     function handleChange(e) {
         setState({
@@ -110,7 +159,7 @@ export default function PostAd() {
 
     return (
         <>
-        <link rel = "shortcut icon" type="image/png" href="../../image/favicon-32x32.png"></link>
+        {/* <link rel = "shortcut icon" type="image/png" href="../../image/favicon-32x32.png"></link>
             <Card>
                 <Card.Body>
                     <Form onSubmit={createAd}>
@@ -145,7 +194,128 @@ export default function PostAd() {
                         <Button disabled={loading} className="w-100" type="submit">Create New Donation Post</Button>
                     </Form>
                 </Card.Body>
-            </Card>
+            </Card> */}
+
+        <Container component="main" maxWidth="xs">
+            <CssBaseline/>
+            <div classname={classes.paper}>
+            <Typography 
+            component="h1" 
+            variant= "h5" c
+            >
+                Create a new donation
+            </Typography>
+            </div>
+            <form className={classes.form} noValidate></form>
+            <TextField
+             variant="outlined"
+             margin="normal"
+             required
+             fullWidth
+             id="title"
+             label="Title"
+             name="Title"
+             autoFocus
+             inputRef={titleRef}
+             />
+
+            <TextField
+             variant="outlined"
+             margin="normal"
+             required
+             fullWidth
+             id="City"
+             label="City/Neighbourhood"
+             name="City"
+             autoFocus
+             inputRef={cityRef}
+             />
+
+             <TextField
+             variant="outlined"
+             margin="normal"
+             required
+             fullWidth
+             id="postalCode"
+             label="Postal Code"
+             name="Postal Code"
+             autoFocus
+             inputRef={postalRef}
+             />
+
+        <TextField
+          id="bottleTotal"
+          name="Bottles"
+          type="number"
+          label="Total bottles"
+          fullWidth
+          required
+          margin="normal"
+          variant="outlined"
+          inputRef={bottleRef}
+        />
+
+            <Grid item xs={12}>
+              <FormControlLabel
+                control={<Checkbox value="plastic" color="primary" />}
+                label="Plastic"
+              />
+              <FormControlLabel
+                control={<Checkbox value="Glass" color="primary" />}
+                label="Glass"
+              />
+              <FormControlLabel
+                control={<Checkbox value="Aluminium" color="primary" />}
+                label="Aluminium"
+              />
+              <FormControlLabel
+                control={<Checkbox value="Other" color="primary" />}
+                label="Other"
+              />
+            </Grid>
+            <TextField
+             variant="outlined"
+             margin="normal"
+             required
+             fullWidth
+             id="description"
+             label="Description"
+             name="Description"
+             autoFocus
+             inputRef={descRef}
+             />
+             
+             <TextField
+             variant="outlined"
+             margin="normal"
+             required
+             fullWidth
+             id="postalCode"
+             label="Contact Info"
+             name="Contact Info"
+             autoFocus
+             inputRef={contactRef}
+             />
+
+        <Form.Group id="pictures">
+        <Form.Control type="file" ref={fileRef} required onChange={handleChange}/>
+        <img src={imageState.file} id="previewImage"/>
+        </Form.Group>
+       
+        <Button
+        type="submit"
+        fullWidth
+        variant="contained"
+        color="primary"
+        className={classes.submit}
+        >
+        Submit Donation
+        </Button>
+
+            </Container>
         </>
+
+       
+
     )
 }
